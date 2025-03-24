712b9b419bd34b9056114751969a42f6e36c925d
lincolnhuls <hul21001@byui.edu>
2025-03-24 14:00:41 -0600

    Added an alt version of the logo for mouse hovers on the logo in the nav

---
+++ 3dprints/js/script.js
@@ -0,0 +1,5 @@
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('mouseover', () => {logo.src = 'images/3dprintslogoalt.png';});
+
+logo.addEventListener('mouseout', () => {logo.src = 'images/3dprintslogo.png';});

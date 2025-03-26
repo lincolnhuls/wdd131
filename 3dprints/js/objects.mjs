8959071c7f1415cd65e6a0d9cc19636bcbddc7ab
lincolnhuls <hul21001@byui.edu>
2025-03-26 14:24:15 -0600

    Added the js for the dynamic element and styling to make it look good

---
+++ 3dprints/js/objects.mjs
@@ -0,0 +1,34 @@
+const objects = [
+    {
+        name: 'Doom Guy',
+        description: 'A large model of the Doom Guy action figure from the Doom Eternal game.',
+        quantity: 1,
+        image: 'images/bigdoomguy.jpeg'
+    },
+    {
+        name: 'Cacodemon',
+        description: 'A medium model of the Cacodemon action figure from the Doom Eternal game.',
+        quantity: 1,
+        image: 'images/cacodemon.jpeg'
+    },
+    {
+        name: 'Maurader',
+        description: 'A medium model of the Maurader action figure from the Doom Eternal game.',
+        quantity: 1,
+        image: 'images/maurader.jpeg'
+    },
+    {
+        name: 'Master Chief(large)',
+        description: 'A large model of the Master Chief from the Halo games.',
+        quantity: 1,
+        image: 'images/bighalo.jpeg'
+    },
+    {
+        name: 'Master Chief(small)',
+        description: 'A small model of the Master Chief from the Halo games.',
+        quantity: 1,
+        image: 'images/smallhalo.jpeg'
+    }
+];
+
+export default objects;
--- 3dprints/js/script.js
+++ 3dprints/js/script.js
@@ -1,3 +1,30 @@
+import objects from './objects.mjs'
+
+function objectTemplate(object) {
+    return `<figure class="object">
+    <img src="${object.image}" alt="image of ${object.name}" />
+    <figcaption>
+        <h2>${object.name}</h2>
+        <p class="object__description">
+            ${object.description}
+        </p>
+        <p>QT: ${object.quantity}</p>
+    </figcaption>
+    </figure>`;
+}
+
+function renderObjects(objectList) {
+    const output = document.querySelector('.objectInfo'); 
+    const objectHtml = objectList.map(objectTemplate).join('');
+    output.innerHTML = objectHtml;
+}
+
+function init() {
+    renderObjects(objects);
+}
+
+init();
+
 const logo = document.querySelector('.logo');
 
 logo.addEventListener('mouseover', () => {logo.src = 'images/3dprintslogoalt.png';});
